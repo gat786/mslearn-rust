50ad4f61defbd60c7d58604fc71d6dc75b3103d1
Ganesh Tiwari <ganesht049@gmail.com>
2021-05-29 19:44:27 +0530

    completed the indexing challenge

---
+++ challenge-indexing/src/challenge.rs
@@ -0,0 +1,23 @@
+fn indexing_tuple() {
+    let numbers = (1, 2, 3);
+    // TODO - Correct the syntax
+    let second = todo!("Replace with the tuple indexing syntax");
+
+    assert_eq!(
+        2, second,
+        "This is not the 2nd number in the tuple: {}",
+        second
+    )
+}
+
+fn indexing_array() {
+    let characters = ['a', 'b', 'c', 'd', 'e'];
+    // TODO - Correct the syntax
+    let letter_d = todo!("Replace with the array indexing syntax");
+
+    assert_eq!(
+        'd', letter_d,
+        "This is not the character for the letter d: {}",
+        letter_d
+    )
+}
---
+++ challenge-indexing/src/main.rs
@@ -0,0 +1,6 @@
+mod solution;
+
+fn main() {
+    solution::indexing_array();
+    solution::indexing_tuple();
+}
